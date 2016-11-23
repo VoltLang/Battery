182bc2ab437008a5e66e948b500cb8ca226a908c
Bernard Helyer <b.helyer@gmail.com>
2016-11-24 02:16:59 +1300

    battery: Update sources.mk path.

--- sources.mk
+++ sources.mk
@@ -5,6 +5,6 @@ SRC=\
 	src/battery/policy/*.volt \
 	src/battery/util/*.volt \
 	src/battery/*.volt \
-	src/uni/core/*.volt \
-	src/uni/util/*.volt \
+	src/build/core/*.volt \
+	src/build/util/*.volt \
 	src/*.volt
